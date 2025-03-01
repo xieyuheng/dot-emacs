08326ec86a8cdce8a8802dacbd0cb980b0f4cf18
Xie Yuheng <xyheme@gmail.com>
2025-03-01 22:47:12 +0800

    up

--- configs/config-eshell.el
+++ configs/config-eshell.el
@@ -7,3 +7,9 @@
 (add-hook 'eshell-mode-hook
           (lambda ()
             (local-set-key (kbd "C-s C-e") 'previous-buffer)))
+
+(defun eshell/ll (&rest args)
+  (apply #'eshell/ls (cons "-l" args)))
+
+(defun eshell/e (&rest args)
+  (apply #'find-file args))
