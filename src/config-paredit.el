3e20deb059a9eb5ff576a65c6a32e4d97f781387
Xie Yuheng <xyheme@gmail.com>
2025-03-03 09:29:35 +0800

    up

--- src/config-paredit.el
+++ src/config-paredit.el
@@ -8,19 +8,14 @@
 (add-hook 'shen-mode-hook        'enable-paredit-mode)
 (add-hook 'emacs-lisp-mode-hook  'enable-paredit-mode)
 
-(define-key paredit-mode-map (kbd "M-(")          'paredit-wrap-round)
 (define-key paredit-mode-map (kbd "C-M-9")        'paredit-wrap-round)
 (define-key paredit-mode-map (kbd "M-c")          'paredit-splice-sexp)
 (define-key paredit-mode-map (kbd "M-r")          'paredit-raise-sexp)
 (define-key paredit-mode-map (kbd "<C-right>")    'paredit-forward-slurp-sexp)
 (define-key paredit-mode-map (kbd "<C-left>")     'paredit-forward-barf-sexp)
-(define-key paredit-mode-map (kbd "M-l")          'paredit-forward-slurp-sexp)
-(define-key paredit-mode-map (kbd "M-h")          'paredit-forward-barf-sexp)
 (define-key paredit-mode-map (kbd "M-\"")         'paredit-meta-doublequote)
 (define-key paredit-mode-map (kbd "<C-M-right>")  'paredit-backward-barf-sexp)
 (define-key paredit-mode-map (kbd "<C-M-left>")   'paredit-backward-slurp-sexp)
-(define-key paredit-mode-map (kbd "C-M-l")        'paredit-backward-barf-sexp)
-(define-key paredit-mode-map (kbd "C-M-h")        'paredit-backward-slurp-sexp)
 
 ;; the follow functions are from lisp.el
 
