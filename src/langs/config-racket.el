cc87ebf9b3744450a820aba95a9016f119acb7ef
Xie Yuheng <xyheme@gmail.com>
2025-05-27 18:15:11 +0800

    up

--- src/langs/config-racket.el
+++ src/langs/config-racket.el
@@ -1,3 +1,5 @@
 (add-to-list 'load-path "~/.emacs.d/racket-mode/")
 (require 'racket-mode)
 (setq auto-mode-alist (cons `("\\.rkt$" . racket-mode) auto-mode-alist))
+
+(define-key racket-mode-map (kbd "C-M-y") nil)
