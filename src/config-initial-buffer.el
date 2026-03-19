c079d4e2d1a94a20f95bbd22d7cf240bb283b1b3
Xie Yuheng <xyheme@gmail.com>
2026-03-19 18:11:11 +0800

    up

--- src/config-initial-buffer.el
+++ src/config-initial-buffer.el
@@ -1,4 +1,8 @@
-(if (<= (length command-line-args) 1)
-    (let ((filename "~/inner/TODO.md"))
-      (when (file-exists-p filename)
-        (setq initial-buffer-choice filename))))
+(setq initial-major-mode 'markdown-mode)
+
+;; (if (<= (length command-line-args) 1)
+;;     (let ((filename "~/inner/TODO.md"))
+;;       (when (file-exists-p filename)
+;;         (setq initial-buffer-choice filename))))
+
+
