1d56b000c968c65c7e73b1177ff6dce60008ab82
Xie Yuheng <xyheme@gmail.com>
2026-03-24 17:33:24 +0800

    up

--- src/langs/config-scheme.el
+++ src/langs/config-scheme.el
@@ -13,6 +13,7 @@
 (setq auto-mode-alist (cons `("\\.cicada$" . scheme-mode) auto-mode-alist))
 (setq auto-mode-alist (cons `("\\.machine$" . scheme-mode) auto-mode-alist))
 (setq auto-mode-alist (cons `("\\.dump$" . scheme-mode) auto-mode-alist))
+(setq auto-mode-alist (cons `("\\.stack$" . scheme-mode) auto-mode-alist))
 
 (defun switch-to-buffer-*scheme* ()
   (interactive)
