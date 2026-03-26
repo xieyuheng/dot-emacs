2c844cd65bc7518cf23be57237f3777e774f4efa
Xie Yuheng <xyheme@gmail.com>
2026-03-27 02:44:38 +0800

    up

--- src/config-initial-buffer.el
+++ src/config-initial-buffer.el
@@ -1,8 +1,12 @@
 (setq initial-major-mode 'markdown-mode)
 
+(if (<= (length command-line-args) 1)
+    (let ((filename "~/.emacs.d/scratch.md"))
+      (when (not (file-exists-p filename))
+        (make-empty-file filename))
+      (setq initial-buffer-choice filename)))
+
 ;; (if (<= (length command-line-args) 1)
 ;;     (let ((filename "~/inner/TODO.md"))
 ;;       (when (file-exists-p filename)
 ;;         (setq initial-buffer-choice filename))))
-
-
