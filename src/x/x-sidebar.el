a738acca217a4775d0626c2bb034475364a06339
Xie Yuheng <xyheme@gmail.com>
2026-06-03 09:39:22 +0800

    x-sidebar: fix split-window size to preserve sidebar width

--- src/x/x-sidebar.el
+++ src/x/x-sidebar.el
@@ -109,7 +109,7 @@
 (defun x-sidebar--main-window ()
   (let ((cur (selected-window)))
     (or (cl-find-if (lambda (w) (not (eq w cur))) (window-list))
-        (split-window cur nil 'right))))
+        (split-window cur x-sidebar--width 'right))))
 
 (defun x-sidebar--switch-to-main ()
   "Switch focus to the main window."
